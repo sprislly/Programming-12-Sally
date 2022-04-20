4c979ffb9b113cfc94b6b75b488467384ca34169
sprislly <99052001+sprislly@users.noreply.github.com>
2022-04-20 13:52:30 -0700

    Recursive Fibonacci Sequence Sally

---
+++ Fibonacci Recursive Sally/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    static int sum;
+
+    /*
+        Fibonacci Solution.
+        @param n number in sequence; requires n > 0;
+        @return the solution of the fibonacci sequence
+    */
+
+    public static void sum(int sequence) {
+        if (sequence > 0) {
+            sum += sequence;
+        }
+    }
+
+    /*
+        Fibonacci Sequence.
+        @param n number spot in sequence; requires n > 0;
+        @returns the number in the fibonacci sequence for the specified number spot
+    */
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return n;
+        }
+
+        if (n == 1 || n == 2) {
+            sum(n);
+            return 1;
+        }
+        sum(1);
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+
+    public static void main (String[]args){
+        fibonacci(2);
+        System.out.print(sum);
+    }
+}
