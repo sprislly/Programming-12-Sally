27db3e86c4a4884fb4c151298d00fca84ad8c286
sally <99052001+sprislly@users.noreply.github.com>
2022-05-25 20:03:00 -0700

    Caeser

---
+++ Caesar Cipher Sally/src/Cipher.java
@@ -0,0 +1,17 @@
+public class Cipher {
+    String message;
+    int shift;
+    Cipher(String message, int shift){
+        this.message = message;
+        this.shift = shift;
+    }
+    
+    void Encrypt(){
+        shift;
+    }
+    
+    void Decrypt(){
+        -1 * shift
+    }
+    
+}
---
+++ Caesar Cipher Sally/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cipher test = new Cipher("defend the east wall of the castle", 1);
+        System.out.print(test);
+    }
+
+}
