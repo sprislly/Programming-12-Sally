5cf6cb3674eeaf998744383055496d00d7e47801
sally <99052001+sprislly@users.noreply.github.com>
2022-06-09 14:33:09 -0700

    delete

--- Caesar Cipher Sally/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CaesarCipher code = new CaesarCipher();
-        String c = code.encrypt("defend the east wall of the castle", 1);
-        System.out.println(c);
-        String d = code.decrypt(c, 1);
-        System.out.println(d);
-    }
-}
