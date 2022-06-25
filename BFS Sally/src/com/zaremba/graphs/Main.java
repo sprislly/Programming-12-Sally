be265a479d3bf7cdb0a88ecba4e5e2861d935011
sally <99052001+sprislly@users.noreply.github.com>
2022-06-25 08:59:54 -0700

    BFS

--- BFS Sally/src/com/zaremba/graphs/Main.java
+++ BFS Sally/src/com/zaremba/graphs/Main.java
@@ -6,17 +6,60 @@ import java.util.*;
 
 public class Main {
     public static ArrayList<Node> graph;
+    public static ArrayList<Integer> checked = new ArrayList<>();
     public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("BFS.");
         setupTree();
-        BFS();
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter your root node: ");
+        int root = myObj.nextInt();
+
+        checked.add(graph.get(root-1).getKey());
+        BFS(root-1);
+        for (Integer i : checked) {
+            System.out.println(i);
+        }
 
     }
 
-
-    private static void BFS() {
+    /* BFS
+    The BFS algorithm that checks a node then all the nodes it is connected to.
+    @param int index the starting node for the algorithm; requires int, index > 0.
+    @return checks the line and then connected nodes to make sure they are accounted for in the list.
+     */
+    private static void BFS(int index) {
         //Your code here.  Feel free to modify signature or add helper functions
-
+        Node line = graph.get(index);
+        line.setVisited(true);
+        addNode(line);
+        for (Integer i : checked) {
+            if(!graph.get(i-1).isVisited()) {
+                BFS(i-1);
+                break;
+            }
+        }
     }
+
+    /* addNode
+    @param Node line all the nodes in a line.
+    @return adds the node in the list if it isn't already there.
+     */
+    private static void addNode(Node line){
+        for(Integer i : line.getNodes()){
+            int key = 0;
+            for (Integer j : checked) {
+                if(Objects.equals(i, j)){
+                    key = 1;
+                    break;
+                }
+            }
+            if (key != 1) {
+                checked.add(i);
+            }
+        }
+    }
+
 
     private static void setupTree() throws FileNotFoundException {
         graph = new ArrayList<>();
